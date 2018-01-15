e6436a9d4c081a2d42a39f255deee13ea1c11179
castyourstone <chrisbbenyard@gmail.com>
2018-01-15 09:32:34 -0500

    coudtown
    update

--- Imminent/js/main.js
+++ Imminent/js/main.js
@@ -59,11 +59,9 @@ $('#countdown').countdown({
 	date: "December 13, 2017 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+	  el.append("hero" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+	    .append("hero" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
+	    .append("hero" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
+	    .append("hero" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
 	}
 });
