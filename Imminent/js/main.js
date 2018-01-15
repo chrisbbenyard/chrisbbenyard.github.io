5b0333209097f3ec7b339eab38d21d363b98f144
castyourstone <chrisbbenyard@gmail.com>
2018-01-15 10:17:12 -0500

    ctsdown
    ctsdown

--- Imminent/js/main.js
+++ Imminent/js/main.js
@@ -55,11 +55,13 @@ $(window).load(function(){
 /* END ------------------------------------------------------- */
 
 
-$('#hero').countdown({
+$('#countdown').countdown({
 	date: "December 13, 2017 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
-	  el.append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+	  el.empty()
+	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
 	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
 	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
 	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
