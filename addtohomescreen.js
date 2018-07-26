d35d1608809eb55b2e65758846b6666c5585803d
chrisbbenyard <chrisbbenyard@gmail.com>
2018-07-26 17:52:01 -0400

    gdfgd
    cbdg

--- addtohomescreen.js
+++ addtohomescreen.js
@@ -510,7 +510,7 @@ ath.Class.prototype = {
 
 		// add the action icon
 		message = '<p>' + message.replace(/%icon(?:\[([^\]]+)\])?/gi, function(matches, group1) {
-			return '<span class="https://chrisbbenyard.github.io/ath-action-icon">' + (!!group1 ? group1 : 'icon') + '</span>';
+			return '<span class="/ath-action-icon">' + (!!group1 ? group1 : 'icon') + '</span>';
 		}) + '</p>';
 
 		// create the message container
