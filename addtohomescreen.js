9f775b01b38d71c7fd96f92d426fa3fe9ebdabcb
chrisbbenyard <chrisbbenyard@gmail.com>
2019-11-09 13:13:06 -0500

    Update addtohomescreen.js

--- addtohomescreen.js
+++ addtohomescreen.js
@@ -59,8 +59,8 @@ ath.intl = {
 	},
 
 	en_us: {
-		ios: '<small>To add this web app to the home screen: tap  <img class="action" src="https://chrisbbenyard.github.io/images/action-icon.png">  and then "Add to Home Screen <img class="add" src="https://chrisbbenyard.github.io/images/action-icon-add.png">".</small>',
-		android: 'To add this web app to the home screen open the browser option menu and tap on <strong>Add to homescreen</strong>. <small>The menu can be accessed by pressing the menu hardware button if your device has one, or by tapping the top right menu icon  <img class="action" src="https://chrisbbenyard.github.io/images/action-icon-android.png">.</small>'
+		ios: '<small>To add this web app to the home screen: tap  <img class="action" src="/assets/imgs/Navigation_Action.png">  and then "Add to Home Screen <img class="add" src="/assets/imgs/QuickActions_Add.png">".</small>',
+		android: 'To add this web app to the home screen open the browser option menu and tap on <strong>Add to homescreen</strong>. <small>The menu can be accessed by pressing the menu hardware button if your device has one, or by tapping the top right menu icon %icon.</small>'
 	},
 
 	es_es: {
